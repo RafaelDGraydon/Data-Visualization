935992f9771c8421777996d14912be72280f6dd2
Rafael <rafaeldominguezgraydon@gmail.com>
2021-04-18 18:16:04 +0200

    Añadir Task1

---
+++ Task1/src/index.ts
@@ -0,0 +1,73 @@
+import * as d3 from "d3";
+import { stat } from "fs";
+import * as topojson from "topojson-client";
+const spainjson = require("./spain.json");
+const d3Composite = require("d3-composite-projections");
+import { latLongCommunities } from "./communities";
+import { stats, stats_1, ResultEntry } from "./stats";
+const maxAffected = stats.reduce(
+  (max, item) => (item.value > max ? item.value : max),
+  0
+);
+
+const affectedRadiusScale = d3
+  .scaleLinear()
+  .domain([0, maxAffected])
+  .range([10, 30]);
+
+const calculateRadiusBasedOnAffectedCases = (
+  comunidad: string,
+  dataset: ResultEntry[]
+) => {
+  const entry = dataset.find((item) => item.name === comunidad);
+
+  return entry ? affectedRadiusScale(entry.value) : 0;
+};
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", 1024)
+  .attr("height", 800)
+  .attr("style", "background-color: #FBFAF0");
+
+const aProjection = d3Composite
+  .geoConicConformalSpain()
+  .scale(3300)
+  .translate([500, 400]);
+
+const geoPath = d3.geoPath().projection(aProjection);
+const geojson = topojson.feature(spainjson, spainjson.objects.ESP_adm1);
+
+svg
+  .selectAll("path")
+  .data(geojson["features"])
+  .enter()
+  .append("path")
+  .attr("class", "country")
+  .attr("d", geoPath as any);
+
+const updateChart = (data: ResultEntry[]) => {
+  svg.selectAll("circle").remove();
+  svg
+    .selectAll("circle")
+    .data(latLongCommunities)
+    .enter()
+    .append("circle")
+    .attr("class", "affected-marker")
+    .attr("r", (d) => calculateRadiusBasedOnAffectedCases(d.name, data))
+    .attr("cx", (d) => aProjection([d.long, d.lat])[0])
+    .attr("cy", (d) => aProjection([d.long, d.lat])[1]);
+};
+
+document
+  .getElementById("Init")
+  .addEventListener("click", function ResultsInitCovid() {
+    updateChart(stats);
+  });
+
+document
+  .getElementById("Currently")
+  .addEventListener("click", function ResultsCurrentlyCovid() {
+    updateChart(stats_1);
+  });
---
+++ Task1/src/stats.ts
@@ -0,0 +1,125 @@
+export interface ResultEntry {
+  name: string;
+  value: number;
+}
+
+export const stats: ResultEntry[] = [
+  {
+    name: "Madrid",
+    value: 174,
+  },
+  {
+    name: "La Rioja",
+    value: 39,
+  },
+  {
+    name: "Andalucía",
+    value: 34,
+  },
+  {
+    name: "Cataluña",
+    value: 24,
+  },
+  {
+    name: "Valencia",
+    value: 30,
+  },
+  {
+    name: "Murcia",
+    value: 0,
+  },
+  {
+    name: "Extremadura",
+    value: 6,
+  },
+  {
+    name: "Castilla La Mancha",
+    value: 16,
+  },
+  {
+    name: "País Vasco",
+    value: 45,
+  },
+  {
+    name: "Cantabria",
+    value: 10,
+  },
+  {
+    name: "Asturias",
+    value: 5,
+  },
+  {
+    name: "Galicia",
+    value: 3,
+  },
+  {
+    name: "Aragón",
+    value: 11,
+  },
+  {
+    name: "Castilla y León",
+    value: 19,
+  },
+  {
+    name: "Islas Canarias",
+    value: 18,
+  },
+  {
+    name: "Islas Baleares",
+    value: 6,
+  },
+];
+
+export const stats_1: ResultEntry[] = [
+  { 
+    name: "Andalucía", 
+    value: 212 },
+  { 
+    name: "Aragón", 
+    value: 223 },
+  { 
+    name: "Asturias",
+    value: 163 },
+  { 
+    name: "Baleares", 
+    value: 59 },
+  { 
+    name: "Canarias", 
+    value: 135 },
+  { 
+    name: "Cantabria", 
+    value: 168 },
+  { 
+    name: "Castilla La Mancha", 
+    value: 169 },
+  { 
+    name: "Castilla y León", 
+    value: 197 },
+  { 
+    name: "Cataluña", 
+    value: 213 },
+  { 
+    name: "Valencia", 
+    value: 36 },
+  { 
+    name: "Extremadura", 
+    value: 135 },
+  { 
+    name: "Galicia", 
+    value: 78 },
+  { 
+    name: "Madrid", 
+    value: 341},
+  { 
+    name: "Murcia", 
+    value: 69 },
+  { 
+    name: "Navarra", 
+    value: 426 },
+  { 
+    name: "Pais Vasco", 
+    value: 330 },
+  { 
+    name: "La Rioja", 
+    value:  211},
+];
